25f9d8e0ea89a5681665d2eb29eb9ea73cc85d63
samtech <shyammunde1@gmail.com>
2024-05-10 17:18:27 +0530

    first navbar component created

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import styles from "./Navbar.module.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.circle} style={{ backgroundColor: "#CC2323" }}></div>
+      <div className={styles.circle} style={{ backgroundColor: "#F4BB4A" }}></div>
+      <div className={styles.circle} style={{ backgroundColor: "#48A14D" }}></div>
+    </div>
+  );
 };
 
 export default Navbar;
